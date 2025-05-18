246e31ff55476f55a37615e21c636072f0e48d55
Razurtech <razurtech@pm.me>
2025-05-17 22:59:37 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Hero from "./components/Hero";
-import Bookstore from "./components/Bookstore";
-import Shop from "./components/Shop";
-import Directory from "./components/Directory";
-import Blog from "./components/Blog";
-import Contact from "./components/Contact";
+import Home from "./components/Home";
 
 export default function App() {
   return (
     <Router>
-      <div className="min-h-screen flex flex-col">
-        <Navbar />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Hero />} />
-            <Route path="/bookstore" element={<Bookstore />} />
-            <Route path="/orisha-shop" element={<Shop />} />
-            <Route path="/directory" element={<Directory />} />
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </Router>
   );
 }
